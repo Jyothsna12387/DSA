aad8bf19cbdacbce233ed145114d8a55cae09d7a
Jyothsna12387 <guthujyothsna@gmail.com>
2026-04-26 11:57:51 +0530

    Time: 6 ms (31.3%), Space: 43.8 MB (30.92%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        Stack<Character> st = new Stack<>();
+        StringBuilder res = new StringBuilder();
+        for(char ch : s.toCharArray()){
+            if(ch == '('){
+                if(!st.isEmpty()){
+                    res.append(ch);
+                }
+                st.push(ch);
+            }
+            else if(ch == ')'){
+                st.pop();
+                if(!st.isEmpty()){
+                    res.append(ch);
+                }
+            }
+        }
+        return res.toString();
+    }
+}
