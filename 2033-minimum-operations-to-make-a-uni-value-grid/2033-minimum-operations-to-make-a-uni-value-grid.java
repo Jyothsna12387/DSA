dd7f568a362dc0bf9f37b00a3c492364943c8c9b
Jyothsna12387 <guthujyothsna@gmail.com>
2026-04-28 21:03:00 +0530

    Time: 33 ms (90.08%), Space: 111.2 MB (44.27%) - LeetHub

--- 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
+++ 2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
@@ -1,34 +1,39 @@
+import java.util.Arrays;
+
 class Solution {
     public int minOperations(int[][] grid, int x) {
         int m = grid.length;
         int n = grid[0].length;
         int[] arr = new int[m*n];
         int k = 0;
-        //step 1: flatten array(convert 2D array to 1D array)
-        for(int i = 0; i<m; i++){
-            for(int j=0; j<n; j++){
-                arr[k] = grid[i][j];
-                k++;
+
+        // Step 1: Flatten
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                arr[k++] = grid[i][j];
             }
         }
 
-        //check modulo
+        // Step 2: Modulo check
         int base = arr[0] % x;
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] % x != base){
+        for(int val : arr){
+            if(val % x != base){
                 return -1;
             }
         }
 
+        // Step 3: Sort
         Arrays.sort(arr);
 
+        // Step 4: Correct median
         int median = arr[arr.length / 2];
 
-        //calculate operations
+        // Step 5: Operations
         int operations = 0;
         for(int num : arr){
-            operations += Math.abs(num-median) / x;
+            operations += Math.abs(num - median) / x;
         }
+
         return operations;
     }
 }
