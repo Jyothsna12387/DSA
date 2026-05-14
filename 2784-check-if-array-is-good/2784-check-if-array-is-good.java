c8ed466bc907d674eaaa4c2ad7ddf193535138eb
Jyothsna12387 <guthujyothsna@gmail.com>
2026-05-14 15:17:00 +0000

    Time: 0 ms (100%), Space: 44.4 MB (86.29%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.java
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.java
@@ -1,30 +1,43 @@
 class Solution {
     public boolean isGood(int[] nums) {
+
         int max = 0;
 
-        HashMap<Integer, Integer> map = new HashMap<>();
-
+        // Find maximum element
         for (int num : nums) {
             max = Math.max(max, num);
-            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
         int n = max;
 
-        // length should be n + 1
+        // Length should be n + 1
         if (nums.length != n + 1) {
             return false;
         }
 
+        // Frequency array
+        int[] freq = new int[n + 1];
+
+        // Count frequency
+        for (int num : nums) {
+
+            // invalid number
+            if (num > n) {
+                return false;
+            }
+
+            freq[num]++;
+        }
+
         // 1 to n-1 should appear once
         for (int i = 1; i < n; i++) {
-            if (!map.containsKey(i) || map.get(i) != 1) {
+            if (freq[i] != 1) {
                 return false;
             }
         }
 
         // n should appear twice
-        if (map.get(n) != 2) {
+        if (freq[n] != 2) {
             return false;
         }
 
