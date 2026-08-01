0e2b345624e64d47cf40c6593368e99803d266ea
Jyothsna12387 <guthujyothsna@gmail.com>
2026-08-01 23:11:46 +0530

    Time: 4 ms (87.92%) | Memory: 45.1 MB (39.62%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int solve(int ind, int target, int[] nums){
+    public int solve(int ind, int target, int[] nums, int[][] dp){
         //base case
         if(ind == 0){
             if(target == 0 && nums[0] == 0) return 2;  //element taking=1, empty set(not picking)=1,  total=2 ways
@@ -8,26 +8,31 @@ class Solution {
             else return 0;
         }
 
-        int skip = solve(ind-1, target, nums);
+        if(dp[ind][target] != -1) return dp[ind][target];
+        int skip = solve(ind-1, target, nums, dp);
         int take = 0;
         if(nums[ind] <= target){
-            take = solve(ind-1, target-nums[ind], nums);
+            take = solve(ind-1, target-nums[ind], nums, dp);
         }
-        return skip + take;
+        dp[ind][target] = skip + take;
+        return dp[ind][target];
     }
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
         int totalsum = 0;
         for(int num : nums){
-            totalsum += num;
+             totalsum += num;
         }
-        int s2 = (totalsum + target)/2;
+        int s1 = (totalsum + target)/2;
 
         // Impossible cases
         if (totalsum + target < 0 || (totalsum + target) % 2 != 0) {
             return 0;
         }
-
-        return solve(n-1, s2, nums);
+        int[][] dp = new int[n][s1+1];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return solve(n-1, s1, nums, dp);
     }
 }
