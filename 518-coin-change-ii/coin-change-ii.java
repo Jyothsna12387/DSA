b53f792364e4cf2f1cded34e9325c698fab3f7ea
Jyothsna12387 <guthujyothsna@gmail.com>
2026-08-01 16:55:24 +0530

    Time: 5 ms (99.82%) | Memory: 43.3 MB (66.88%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,29 +1,17 @@
 class Solution {
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-        int[] dp = new int[amount+1];
+        int[] dp = new int[amount + 1];
         
-        //base case(amount == 0)
-            dp[0] = 1;
+        // Base case: 1 way to make amount 0 (using no coins)
+        dp[0] = 1;
         
-        //base case 2(index == 0 which means fill first row)
-        for(int target=1; target<=amount; target++){
-            dp[target] = target % coins[0] == 0 ? 1 : 0;
-        }
-
-        // Fill the remaining table
-        for (int ind = 1; ind < n; ind++) {
-            for (int target = 1; target <= amount; target++) {
-                int notTake = dp[target];
-                int take = 0;
-
-                if (coins[ind] <= target) {
-                    take = dp[target - coins[ind]];
-                }
-                dp[target] = take+notTake;
+        // Iterate over each coin first to ensure unique combinations (no permutations)
+        for (int coin : coins) {
+            for (int target = coin; target <= amount; target++) {
+                dp[target] = dp[target] + dp[target - coin];  //skip+take
             }
         }
-        int ans = dp[amount];
-        return ans;
+        
+        return dp[amount];
     }
 }
