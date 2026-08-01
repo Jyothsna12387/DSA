0b47a7c86bc903bfd045771516c6444a0fbf31bd
Jyothsna12387 <guthujyothsna@gmail.com>
2026-08-01 17:33:10 +0530

    Time: 8 ms (68.91%) | Memory: 42.9 MB (90.69%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,17 +1,26 @@
 class Solution {
     public int change(int amount, int[] coins) {
+        int n = coins.length;
         int[] dp = new int[amount + 1];
         
         // Base case: 1 way to make amount 0 (using no coins)
         dp[0] = 1;
         
+        for(int target=1; target<=amount; target++){
+            dp[target] = (target % coins[0] == 0) ? 1 : 0;
+        }
+
         // Iterate over each coin first to ensure unique combinations (no permutations)
-        for (int coin : coins) {
-            for (int target = coin; target <= amount; target++) {
-                dp[target] = dp[target] + dp[target - coin];  //skip+take
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 1; target <= amount; target++) {
+                int notTake = dp[target];
+                int take = 0;
+                if (coins[ind] <= target) {
+                    take = dp[target - coins[ind]];
+                }
+                dp[target] = take + notTake;
             }
         }
-        
         return dp[amount];
     }
 }
