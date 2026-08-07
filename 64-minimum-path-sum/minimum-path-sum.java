d60e5dee0c1f6ef4414890a6bb8ed665540fe795
Jyothsna12387 <guthujyothsna@gmail.com>
2026-08-07 13:36:08 +0530

    Time: 5 ms (25.38%) | Memory: 50.5 MB (21.66%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -4,27 +4,26 @@ class Solution {
         int n = grid[0].length;
         int[] prev = new int[n];
 
-        for(int i=0; i<m; i++){
+        for(int i = 0; i < m; i++) {
             int[] curr = new int[n];
-            for(int j=0; j<n; j++){
-                if(i==0 && j==0){
-                    curr[0] = grid[0][0];
-                     continue;
+            for(int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) {
+                    curr[j] = grid[0][0];
+                    continue;
                 }
 
-                int up = (int) 1e9;
-                if(i>0) {
-                   up = grid[i][j] + prev[j];
-                 }
+                int up = (int)1e9;
+                if(i > 0)
+                    up = grid[i][j] + prev[j];
 
-                int left = (int) 1e9;
-                if(j>0) {
-                   left = grid[i][j] + curr[j-1];
-                 }
-             curr[j] = Math.min(up, left);
+                int left = (int)1e9;
+                if(j > 0)
+                    left = grid[i][j] + curr[j - 1];
+
+                curr[j] = Math.min(up, left);
             }
             prev = curr;
         }
-        return prev[n-1];
+        return prev[n - 1];
     }
 }
