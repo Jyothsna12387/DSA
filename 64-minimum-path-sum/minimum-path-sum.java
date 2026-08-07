2632b39ef0624a18672a68b2bad302fdbed7f8c7
Jyothsna12387 <guthujyothsna@gmail.com>
2026-08-07 13:28:23 +0530

    Time: 6 ms (10.23%) | Memory: 50.2 MB (57.02%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,17 +1,4 @@
 class Solution {
-    public int solve(int i, int j, int[][] grid, int[][] dp){
-        if(i<0 || j<0) return (int) 1e9;
-        if(i == 0  && j == 0) return grid[0][0];
-
-        if(dp[i][j] != -1) return dp[i][j];
-        //try all possibilities  
-        int up = grid[i][j] + solve(i-1, j, grid, dp);
-
-        int left = grid[i][j] + solve(i, j-1, grid, dp);
-
-        return dp[i][j] = Math.min(up, left);
-        
-    }
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
