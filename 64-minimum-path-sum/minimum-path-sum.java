303664d953fe3596060add2588adc674991fc3de
Jyothsna12387 <guthujyothsna@gmail.com>
2026-08-07 13:04:40 +0530

    Time: 1 ms (99.90%) | Memory: 50.4 MB (38.89%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,29 +1,25 @@
 class Solution {
+    public int solve(int i, int j, int[][] grid, int[][] dp){
+        if(i<0 || j<0) return (int) 1e9;
+        if(i == 0  && j == 0) return grid[0][0];
+
+        if(dp[i][j] != -1) return dp[i][j];
+        //try all possibilities  
+        int up = grid[i][j] + solve(i-1, j, grid, dp);
+
+        int left = grid[i][j] + solve(i, j-1, grid, dp);
+
+        return dp[i][j] = Math.min(up, left);
+        
+    }
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-
         int[][] dp = new int[m][n];
-        dp[0][0] = grid[0][0];
-
-        //populate first row
-        for(int i=1; i<n; i++){
-            dp[0][i] = grid[0][i] + dp[0][i-1];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
         }
 
-        //populate first column
-        for(int i=1; i<m; i++){
-            dp[i][0] = grid[i][0] + dp[i-1][0];
-        }
-
-        //fill remaining cells
-        for(int i=1; i<m; i++){
-            for(int j=1; j<n; j++){
-                int top = dp[i-1][j];
-                int left = dp[i][j-1];
-                dp[i][j] = grid[i][j] + Math.min(top, left);
-            }
-        }
-        return dp[m-1][n-1];
+        return solve(m-1, n-1, grid, dp);
     }
 }
